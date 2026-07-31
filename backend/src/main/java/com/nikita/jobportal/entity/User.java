2fafe323f46966ba3979891535c545615e82cfbc
Nikita Bharti <nikitabharti726@gmail.com>
2026-07-31 22:57:22 +0530

    Create User.java

---
+++ backend/src/main/java/com/nikita/jobportal/entity/User.java
@@ -0,0 +1,31 @@
+package com.nikita.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+}
