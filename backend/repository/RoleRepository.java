6f172df10d2467ac05d2175cf488cbdd9865eb7a
Nikita Bharti <nikitabharti726@gmail.com>
2026-08-04 23:13:39 +0530

    Create RoleRepository.java

---
+++ backend/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.nikita.jobportal.repository;
+
+import com.nikita.jobportal.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+}
