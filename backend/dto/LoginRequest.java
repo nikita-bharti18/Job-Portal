b213cd4b56a9d99ff28eb60b3fc6590d727fdd6c
Nikita Bharti <nikitabharti726@gmail.com>
2026-08-07 23:01:36 +0530

    Create LoginRequest.java

---
+++ backend/dto/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.nikita.jobportal.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
