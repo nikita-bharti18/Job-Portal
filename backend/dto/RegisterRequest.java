4247e2a536c177e2d3a67cc6adad6ef218e2fdcc
Nikita Bharti <nikitabharti726@gmail.com>
2026-08-08 23:29:36 +0530

    Create RegisterRequest.java

---
+++ backend/dto/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.nikita.jobportal.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String role;
+
+}
