57e910eb912eee44f62ad9e2309a39969bd2b31a
Nikita Bharti <nikitabharti726@gmail.com>
2026-08-29 23:14:58 +0530

    Add ResourceNotFoundException class

---
+++ backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.nikita.jobportal.exception;
+
+public class ResourceNotFoundException
+        extends RuntimeException {
+
+    public ResourceNotFoundException(
+            String message) {
+
+        super(message);
+    }
+}
