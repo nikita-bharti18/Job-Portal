cb88f3cb9b8696568ea1588b7f50ba265594bb14
Nikita Bharti <nikitabharti726@gmail.com>
2026-09-06 21:24:20 +0530

    Create AdminController.java

---
+++ backend/src/main/java/com/nikita/jobportal/controller/AdminController.java
@@ -0,0 +1,63 @@
+package com.nikita.jobportal.controller;
+
+import com.nikita.jobportal.entity.User;
+import com.nikita.jobportal.repository.UserRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+
+    private final UserRepository userRepository;
+
+    public AdminController(
+            UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        return ResponseEntity.ok(
+                userRepository.findAll()
+        );
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                userRepository.findById(id)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "User not found"
+                                ))
+        );
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(
+            @PathVariable Long id) {
+
+        if (!userRepository.existsById(id)) {
+
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+
+        userRepository.deleteById(id);
+
+        return ResponseEntity.ok(
+                "User deleted successfully"
+        );
+    }
+}
